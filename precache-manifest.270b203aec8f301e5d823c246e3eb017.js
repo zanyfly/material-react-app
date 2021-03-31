36481d478abac0a8ff3c2ae371af220f87bb4c5f
zanyfly <zanyfly@users.noreply.github.com>
2021-03-31 01:13:34 +0000

    Deploying to gh-pages from  @ b7071a241bfff295e1722f7da676c276cfe18c91 🚀

---
+++ precache-manifest.270b203aec8f301e5d823c246e3eb017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088e23369338f4e59052a8d95f1752c1",
+    "url": "/material-react-app/index.html"
+  },
+  {
+    "revision": "619c322eab0884796a0b",
+    "url": "/material-react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7e3dc2f850c38c40517b",
+    "url": "/material-react-app/static/js/2.67e771ef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/material-react-app/static/js/2.67e771ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "619c322eab0884796a0b",
+    "url": "/material-react-app/static/js/main.0cf5abe9.chunk.js"
+  },
+  {
+    "revision": "4d672b1535ae99418870",
+    "url": "/material-react-app/static/js/runtime-main.51d4e986.js"
+  }
+]);
